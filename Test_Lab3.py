5964ed999890602fd3ec43ca9eae2e3c9587055f
Jonathan <JONATHANKHOO.22@ichat.sp.edu.sg>
2023-05-08 16:16:02 +0800

    New

--- Test_Lab3.py
+++ Test_Lab3.py
@@ -3,28 +3,27 @@ import Lab3
 print("Test_Lab3")
 
 
-def test_bubble_sort_ascending():
-    result = []
-    input_arr = [64, 34, 25, 12, 22, 11, 90]
-    test_arr = [11, 12, 22, 25, 34, 64, 90]
+def calculate_bmi(height, weight):
+    bmi = weight / (height * height)
+    if bmi < 18.5:
+        return bmi, -1  # Under weight
+    elif bmi >= 18.5 and bmi < 25:
+        return bmi, 0   # Normal weight
+    else:
+        return bmi, 1   # Over weight
 
-    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+# Get user input for height and weight
+height = float(input("Enter height in meters: "))
+weight = float(input("Enter weight in kilograms: "))
 
-    assert (result == test_arr)
+# Call the calculate_bmi function with user input
+bmi, bmi_classification = calculate_bmi(height, weight)
 
-def test_bubble_sort_descending():
-    result = []
-    input_arr = [64, 34, 25, 12, 22, 11, 90]
-    test_arr = [90, 64, 34, 25, 22, 12, 11]
-
-    result = Lab3.bubble_sort(input_arr, Lab3.SORT_DESCENDING)
-
-    assert (result == test_arr)
-
-def test_bubble_sort_invalid():
-    result = []
-    input_arr = [64, 34, 25, 12, 22, 11, 90]
-
-    result = Lab3.bubble_sort(input_arr, 3)
-
-    assert (result == [])
+# Print the calculated BMI and classification
+print("BMI: {:.2f}".format(bmi))
+if bmi_classification == -1:
+    print("Under weight")
+elif bmi_classification == 0:
+    print("Normal weight")
+else:
+    print("Over weight")
